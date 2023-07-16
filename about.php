c8a5fb43b8408076aa0dd173de9ba7010d306909
Chau <chau@gmail.com>
2023-07-16 17:11:15 +0700

    Update About Page

--- about.php
+++ about.php
@@ -14,7 +14,7 @@ $user_id = $_SESSION['user_id']??"";
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Horizon | Về chúng tôi</title>
+   <title>Horizon | Chúng tôi</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
